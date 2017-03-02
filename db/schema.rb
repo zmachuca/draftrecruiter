c7328f873f3eeb7b3e6d474cd20080c2c05af419
Zach Machuca <zachmachuca@gmail.com>
2017-03-01 21:48:15 -0500

    Added Name and Usernames

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302015225) do
+ActiveRecord::Schema.define(version: 20170302023830) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170302015225) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
