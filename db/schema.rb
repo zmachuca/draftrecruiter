1bd85916dd10cc0de4a5e09b94a1d06f62b56bc4
Zach Machuca <zachmachuca@gmail.com>
2017-03-06 22:37:01 -0500

    Added location to posts, added location dropdown menu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307011155) do
+ActiveRecord::Schema.define(version: 20170307033129) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170307011155) do
     t.integer  "user_id"
     t.string   "requirement"
     t.string   "industry"
+    t.string   "location"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
