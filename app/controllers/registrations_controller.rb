bb0ec8bce20a5e3cda0d7d51eeb78d6ae931fa49
Zach Machuca <zachmachuca@gmail.com>
2017-03-07 19:05:10 -0500

    Added company to users, added company dropdown to signup view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :current_password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :company, :password, :current_password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :username, :email, :password, :current_password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :copmany, :password, :current_password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307033129) do
+ActiveRecord::Schema.define(version: 20170307234631) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170307033129) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "username"
+    t.string   "company"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
