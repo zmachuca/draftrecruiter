d12f4599970067cc84762ec89795d585b7dba999
Zach Machuca <zachmachuca@gmail.com>
2017-03-21 20:11:10 -0400

    Added search bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,11 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order("created_at DESC")
-    respond_with(@posts)
+    if params[:search]
+      @posts = Post.search(params[:search]).order("created_at DESC")
+    else
+      @posts = Post.all.order("created_at DESC")
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :resumes
+
+	def self.search(search)
+		if search
+  		where("title LIKE ? OR location LIKE ? OR industry LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+		end
+	end
 end
