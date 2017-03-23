8a5ea37858526bca8a751ace0f8bfe06d46f262b
Zach Machuca <zachmachuca@gmail.com>
2017-03-22 21:20:32 -0400

    Changed authentication to index only and not both index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
   respond_to :html
 
