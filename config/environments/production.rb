caa79595601858644e00ec50cc8a17f843ddadd6
Zach Machuca <zachmachuca@gmail.com>
2017-05-06 16:17:53 -0400

    Trying to fix Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.mailgun.org",
     port: 587,
+    domain: ENV["DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["MAILGUN_USERNAME"],
