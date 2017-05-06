57f49c63e476dd3c9f8ca459dbf13274671e1a54
Zach Machuca <zachmachuca@gmail.com>
2017-05-06 18:34:29 -0400

    Trying to fix Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Required for Heroku
   # Note to set this to your actual hostname
-  config.action_mailer.default_url_options = { host: 'www.jobfill.co', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'http://jobfill.herokuapp.com/', protocol: 'https' }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
