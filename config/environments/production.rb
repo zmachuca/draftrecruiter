a06291f2fd67777fd4c19739b971c79e5c789769
Zach Machuca <zachmachuca@gmail.com>
2017-07-02 11:49:57 -0400

    trying to fix smtp on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,8 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"],
+    openssl_verify_mode: 'none'
   }
 
   config.paperclip_defaults = {
