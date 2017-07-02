71a4bbb6cfbf02c266ef162dd918174bcc7fb3b9
Zach Machuca <zachmachuca@gmail.com>
2017-07-02 11:29:01 -0400

    Added smtp gamil to mailer

--- app/mailers/resume_mailer.rb
+++ app/mailers/resume_mailer.rb
@@ -1,5 +1,5 @@
 class ResumeMailer < ApplicationMailer
-  default from: "postmaster@mg.jobfill.co"
+  default from: "jobfillteam@gmail.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
